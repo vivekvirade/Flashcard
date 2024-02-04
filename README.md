e2d158c1f0e90840ceae46c5f391cb7d1478cb23
vivek virade <vvirade@gmail.com>
2024-02-04 22:03:53 +0530

    Your commit message here

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Flashcard
+# Flashcard 
+// New Update
---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FlashCardsHome from './Components/Create FlashCards/FlashCardsHome';
+
+function App() {
+  return (
+    <div>
+      
+      <FlashCardsHome />
+    </div>
+  );
+}
+
+export default App;
